c4b8bffb11191084a246971fc2d35bac4fa21519
marsar <marsar24@terpmail.umd.edu>
2020-05-16 03:03:08 -0400

    CNN Architecture

--- Module1_IntroductionToComputerVision/ProjectOne/models.py
+++ Module1_IntroductionToComputerVision/ProjectOne/models.py
@@ -19,17 +19,19 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel: 
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel: after pool: 32-5 + 1 = 28 -> so (32, 14, 14)
         self.conv1 = nn.Conv2d(1, 32, 5)
         
         # 2. Second convolutional Layer
-        self.conv2 = nn.Conv2d(32, 24, 3)
+        # 32 inputs, 64 outputs, kernel size 3.
+        # after pool: 14-3 + 1 = 12 -> so (64, 6, 6)
+        self.conv2 = nn.Conv2d(32, 64, 3)
         
         # 3. MaxPool Layer
         self.pool = nn.MaxPool2d(2,2)
         
-        # 4. Linear Layer One
-        self.f1 = nn.Linear(250*3*3, 42)
+        # 4. Linear Layer One --> 
+        self.f1 = nn.Linear(387200, 42)
         
         # 5. A drop out layer between Dense Layers
         self.df1 = nn.Dropout(p=0.4)
@@ -46,7 +48,7 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+#         x = self.pool(F.relu(self.conv2(x)))
         
         # Flatten
         x = x.view(x.size(0), -1)
