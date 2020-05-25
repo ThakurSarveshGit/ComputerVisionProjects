6bb73835f1fdb26a54269d763efef293ebac400c
marsar <marsar24@terpmail.umd.edu>
2020-05-25 19:41:59 -0400

    Facial Keypoints Recognition

--- Module1_IntroductionToComputerVision/ProjectOne/models.py
+++ Module1_IntroductionToComputerVision/ProjectOne/models.py
@@ -1,63 +1,62 @@
-## TODO: define the convolutional neural network architecture
+# CNN Architecture for Facial Keypoints Detection
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-# can use the below import should you choose to initialize the weights of your Net
-import torch.nn.init as I
+import torch.nn.init as I # can use the below import should you choose to initialize the weights of your Net
 
 
 class Net(nn.Module):
 
     def __init__(self):
-        super(Net, self).__init__()
+        super(Net, self).__init__() # Initialize the Pytorch NN module, parent class of Net class.
         
-        ## TODO: Define all the layers of this CNN, the only requirements are:
-        ## 1. This network takes in a square (same width and height), grayscale image as input
-        ## 2. It ends with a linear layer that represents the keypoints
-        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        # Define a CNN architecture; Initialize custom layers
         
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel: after pool: 32-5 + 1 = 28 -> so (32, 14, 14)
+        # Convolution Layer 1: Input channel: 224x224 images. Output channel: 32. Filter: 5x5. Output Size: (224-5)/1 + 1 = 220
         self.conv1 = nn.Conv2d(1, 32, 5)
-        
-        # 2. Second convolutional Layer
-        # 32 inputs, 64 outputs, kernel size 3.
-        # after pool: 14-3 + 1 = 12 -> so (64, 6, 6)
-        self.conv2 = nn.Conv2d(32, 64, 3)
-        
-        # 3. MaxPool Layer
-        self.pool = nn.MaxPool2d(2,2)
-        
-        # 4. Linear Layer One --> 
-        self.f1 = nn.Linear(387200, 42)
-        
-        # 5. A drop out layer between Dense Layers
-        self.df1 = nn.Dropout(p=0.4)
-        
-        # 6. Linear Layer Two
-        self.f2 = nn.Linear(42, 136)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32,64,3)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(64,128,3)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        self.conv4 = nn.Conv2d(128,256,3)
+        self.pool4 = nn.MaxPool2d(2, 2)
+        self.conv5 = nn.Conv2d(256,512,1)
+        self.pool5 = nn.MaxPool2d(2, 2)
+        
+        self.fc1 = nn.Linear(512*6*6, 1024)
+        self.fc2 = nn.Linear(1024, 136)
+        
+
+        self.drop1 = nn.Dropout(p = 0.1)
+        self.drop2 = nn.Dropout(p = 0.2)
+        self.drop3 = nn.Dropout(p = 0.25)
+        self.drop4 = nn.Dropout(p = 0.25)
+        self.drop5 = nn.Dropout(p = 0.3)
+        self.drop6 = nn.Dropout(p = 0.4)
         
 
         
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
-        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        x = self.pool(F.relu(self.conv1(x)))
-#         x = self.pool(F.relu(self.conv2(x)))
+        # Feed Forward Behavior where x is the input image
         
-        # Flatten
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.drop1(x)
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.drop2(x)
+        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.drop3(x)
+        x = self.pool4(F.relu(self.conv4(x)))
+        x = self.drop4(x)
+        x = self.pool5(F.relu(self.conv5(x)))
+        x = self.drop5(x)
         x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.drop6(x)
+        x = self.fc2(x)
         
-        # Two Linear layers with dropout in between
-        x = F.relu(self.f1(x))
-        x = self.df1(x)
-        x = self.f2(x)
-        
-        
-        # a modified x, having gone through all the layers of your model, should be returned
-        return x
+        return x  # a modified x, having gone through all the layers of your model, should be returned
